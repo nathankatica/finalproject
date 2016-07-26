6b9692661da974c8bada40f819eb7b7e4becc1a0
nathankatica <nathankatica@gmail.com>
2016-07-26 17:10:53 +0200

    Update learn_french.py

--- learn_french.py
+++ learn_french.py
@@ -1,185 +1,83 @@
+#Boilerplate 
 import csv 
 import random
 import sys
 import pprint as pp
 
+#Variable setup
 filename=sys.argv[1]
 wordDict={}
-catList=[]
-
-for line in open(filename,'r'):
-	if 'Category' not in line:
-		wordDict[line.split(';')[0]]=line.split(';')[1].strip('\n')
-	else:
-		catList.append(line.split(';'))
-betterCat=[]
-for splitline in catList:
-	betterCat.append(splitline[1].strip('\n'))
-
-def finalFunc(lb,ub):
-	for i,line in enumerate(open(filename,'r')):
-		if i in range(lb+1,ub):
-			print(line.split(';')[0])
-
+splitList=[]
 iList=[]
 bList=[]
 cList=[]
 correct = 0
-counter = 0
 a = ''
 
-mode_selection = input("Which mode would you like: Practice or Test? ")
+#Create a dictionary with keys of terms and values of answers
+#Create a list of lists of splitlines [Category, NAME\n]
+for line in open(filename,'r'):
+	if 'Category' not in line:
+		wordDict[line.split(';')[0]]=line.split(';')[1].strip('\n')
+	else:
+		splitList.append(line.split(';'))
 
-#PRACTICE MODE
-if mode_selection.lower() == 'practice':
-	print("\nIn practice mode, user can practice as many times as they want.")
-	print("If you wish to leave, select category QUIT when choosing a category.\n")
-	input("Are you ready? Press enter to begin.")
+#Create a list of categories 
+catList=[]
+for splitline in splitList:
+	catList.append(splitline[1].strip('\n'))
 
-	while a != 'quit':
-		print("\nSEARCH FOR WORD")
-		for i in betterCat:
-			print(i)
-		print("QUIT\n")
+#Helper function that takes a range as an argument and prints lines in that range
+def printLineRange(lb,ub):
+	for i,line in enumerate(open(filename,'r')):
+		if i in range(lb+1,ub):
+			print(line.split(';')[0])
 
-		cat_selection=input("Which category would you like to select? ")
-		
-		if cat_selection.lower() == 'quit':
-			a = 'quit'
-			print('Grade Report:', correct, '/', counter)
-
-		elif cat_selection.upper() in betterCat[0:len(betterCat)-1]:
-			for i,cat in enumerate(betterCat):
-				if cat_selection.upper() in cat:
-					iList.append(i)
-					iList.append(i+1)
-			for i,fileline in enumerate(open(filename,'r')):
-				if betterCat[iList[0]] in fileline:
-					bList.append(i)
-				if betterCat[iList[1]] in fileline:
-					bList.append(i)
-			finalFunc(bList[0],bList[1])
-	
-			selection=input("Which word would you like to practice? ").lower()
-			if selection in wordDict.keys():
-				answer=input("What is the correct answer? ").lower()
-				if answer == wordDict[selection]:
-					print("Correct!")
-				else:
-					print("Sorry, answer is: ",wordDict[selection])
-			else:
-				print("Sorry, that word isn't stored. Please try again.")
-			counter += 1
-
-		elif cat_selection.upper() in betterCat:
-			print('past participles is working')
-			for i,line in enumerate(open(filename,'r')):
-				cList.append(i)
-				if cat_selection.upper() in line:
-					iList.append(i)
-			iList.append(cList[len(cList)-1])
-			finalFunc(iList[0],iList[1])
-	
-			selection=input("Please enter your selection here: ").lower()
-			if selection in wordDict.keys():
-				answer=input("What is the correct answer? ").lower()
-				if answer == wordDict[selection]:
-					correct += 1
-					print("Correct!")
-				else:
-					print("Sorry, answer is: ",wordDict[selection])
-			else:
-				print("Sorry, that word isn't stored. Please try again.")
-			counter += 1
-
-		elif cat_selection.upper() == "SEARCH FOR A WORD":
-			wordsearch=input("Enter word here: ")
-			for line in open(filename,'r'):
-					if wordsearch in line:
-						guess=input("Enter answer here: ")
-						if guess == line.split(';')[1].strip():
-							print("Correct!")
-						else:
-							print("Sorry, answer is: ", line.split(';')[1])
-			counter += 1
-
+#Helper function that prompts the user and generates a response based on whether the user's answer is correct
+def wordQuiz(word_selection):
+	if word_selection in wordDict.keys():
+		answer=input("What is the correct answer? ").lower()
+		if answer == wordDict[word_selection]:
+			print("Correct!")
+			return 1
 		else:
-			print("Sorry, that category isn't stored. Please try again.")
+			print("Sorry, answer is: ",wordDict[word_selection])
+	else:
+		print("Sorry, that word isn't stored. Please try again.")
+	return 0
 
-		del iList[0:]
-		del bList[0:]
-		del cList[0:]	
+#Helper function that prints all the term choices for a category 
+#and then calls wordQuiz passing user input as the argument
+def printCategoryTerms(cat_selection):	
 
-
-
-
-#TEST MODE
-if mode_selection.lower() == 'test':
-	print("\nTest mode consists of 10 rounds.\n")
-	input("Are you ready? Press enter to begin.")
-
-	while counter < 10:
-		print("\nSEARCH FOR WORD")
-		for i in betterCat:
-			print(i)
-		print("\n")	
-
-		cat_selection=input("Which category would you like to select? ")
-		
-		if cat_selection.upper() in betterCat[0:len(betterCat)-1]:
-			for i,cat in enumerate(betterCat):
+		#If the category selection is not the last term in the category list
+		if cat_selection.upper() in catList[0:len(catList)-1]:
+			for i,cat in enumerate(catList):
 				if cat_selection.upper() in cat:
 					iList.append(i)
 					iList.append(i+1)
 			for i,fileline in enumerate(open(filename,'r')):
-				if betterCat[iList[0]] in fileline:
+				if catList[iList[0]] in fileline:
 					bList.append(i)
-				if betterCat[iList[1]] in fileline:
+				if catList[iList[1]] in fileline:
 					bList.append(i)
-			finalFunc(bList[0],bList[1])
-	
-			selection=input("Which word would you like to practice? ").lower()
-			if selection in wordDict.keys():
-				answer=input("What is the correct answer? ").lower()
-				if answer == wordDict[selection]:
-					print("Correct!")
-				else:
-					print("Sorry, answer is: ",wordDict[selection])
-				counter += 1
-			else:
-				print("Sorry, that word isn't stored. Please try again.")
+			printLineRange(bList[0],bList[1])
 
-
-		elif cat_selection.upper() in betterCat:
+		#If the category selection is the last term in the category list, 
+		#it cannot use the index of the next Category;NAME from the enumerated lines 
+		#of the file as an upper bound. 
+		#Therefore, the highest index in the enumerated lines of the file is used.
+		#More precisely, it uses the length of a list of all the indices minus one.
+		elif cat_selection.upper() in catList:
 			for i,line in enumerate(open(filename,'r')):
 				cList.append(i)
 				if cat_selection.upper() in line:
 					iList.append(i)
 			iList.append(cList[len(cList)-1])
-			finalFunc(iList[0],iList[1])
-	
-			selection=input("Please enter your selection here: ").lower()
-			if selection in wordDict.keys():
-				answer=input("What is the correct answer? ").lower()
-				if answer == wordDict[selection]:
-					print("Correct!")
-				else:
-					print("Sorry, answer is: ",wordDict[selection])
-				counter += 1	
-			else:
-				print("Sorry, that word isn't stored. Please try again.")
+			printLineRange(iList[0],iList[1])
 
 		elif cat_selection.upper() == "SEARCH FOR A WORD":
-			wordsearch=input("Enter word here: ")
-			for line in open(filename,'r'):
-					if wordsearch in line:
-						guess=input("Enter answer here: ")
-						if guess == line.split(';')[1].strip():
-							correct += 1
-							print("Correct!")
-						else:
-							print("Sorry, answer is: ", line.split(';')[1])
-			counter += 1
+			wordQuiz(input("Enter word here: "))
 
 		else:
 			print("Sorry, that category isn't stored. Please try again.")
@@ -188,4 +86,43 @@ if mode_selection.lower() == 'test':
 		del bList[0:]
 		del cList[0:]
 
+mode_selection = input("Which mode would you like: Practice or Test? ")
+
+#PRACTICE MODE
+if mode_selection.lower() == 'practice':
+	counter = 0
+	correct = 0
+	print("\nIn practice mode, user can practice as many times as they want.")
+	print("If you wish to leave, select category QUIT when choosing a category.\n")
+	input("Are you ready? Press enter to begin.")
+	print("\nSEARCH FOR WORD")
+	for i in catList:
+		print(i)
+	print("QUIT\n")
+	chosen_category=''
+	while chosen_category != 'quit':
+		chosen_category=input("Which category would you like to select? ")
+		if chosen_category != 'quit':
+			printCategoryTerms(chosen_category)
+			correct += wordQuiz(input("Which word would you like to practice? ").lower())
+			counter += 1
+	print('Grade Report:', correct, '/', counter)
+
+
+
+
+#TEST MODE
+elif mode_selection.lower() == 'test':
+	counter = 0
+	print("\nTest mode consists of 10 rounds.\n")
+	input("Are you ready? Press enter to begin.")
+
+	while counter < 10:
+		print("\nSEARCH FOR WORD")
+		for i in catList:
+			print(i)
+		print("\n")	
+		printCategoryTerms(input("Which category would you like to select? "))
+		correct += wordQuiz(input("Which word would you like to practice? ").lower())
+		counter += 1
 	print('Grade Report:', correct, '/', counter)
